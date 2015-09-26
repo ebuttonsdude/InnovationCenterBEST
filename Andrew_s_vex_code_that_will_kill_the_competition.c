dd7e9ee802db671195a0bbba831e9c732a7780dc
Andrew <acsandrewshearer@gmail.com>
2015-09-26 13:03:41 -0600

    added control loop
    ^^

--- Andrew_s_vex_code_that_will_kill_the_competition.c
+++ Andrew_s_vex_code_that_will_kill_the_competition.c
@@ -1,8 +1,24 @@
 
 task main()
 {
-	motor[port6]=127;
-	motor[port4]=127;
+	get_controls();
+}
 
-
+void get_controls() {
+		if (vexRT[Btn6D] == 1) {
+		bool clawclose = true;
+		} else {
+		bool clawclose = false;
+	}
+	if (vexRT[Btn6U] == 1) {
+		bool clawopen = true;
+		} else {
+		bool clawopen = false;
+	}
+	if (clawopen == true) {
+		motor[port4]=127;
+		motor[port7]=127;
+	} else if (clawclose == true) {
+		motor[port4]=-127;
+		motor[port7]=-127;
 }
