c6f1ddc7e16313e5abb194c62966bacddf03f58c
Andrew <acsandrewshearer@gmail.com>
2015-09-26 14:22:22 -0600

    stuff
    stuff

--- Andrew_s_vex_code_that_will_kill_the_competition.c
+++ Andrew_s_vex_code_that_will_kill_the_competition.c
@@ -1,11 +1,8 @@
-
-task main()
+bool clawclose=false;
+bool clawopen=false;
+// need to return functions!!!!
+void get_controls()
 {
-	get_controls();
-	motorz();
-}
-
-void get_controls() {
 	if (vexRT[Btn6D] == 1) {
 		bool clawclose = true;
 		} else {
@@ -16,14 +13,19 @@ void get_controls() {
 		} else {
 		bool clawopen = false;
 	}
-}
-
-void motorz() {
-	if (clawopen == true) {
+	if (vexRT[Btn6U] == 1) {
 		motor[port4]=127;
 		motor[port7]=127;
-		} else if (clawclose == true) {
+		} else if (vexRT[Btn6D] == 1) {
 		motor[port4]=-127;
 		motor[port7]=-127;
+	} else {
+		motor[port4]=0;
+		motor[port7]=0;
 	}
+}
+
+task main()
+{
+	get_controls();
 }
